9e85d04d988cdf42e6f8501361cd8b79528c9c2c
Julianms11 <juliandavidms2004@gmail.com>
2022-06-27 16:21:17 -0500

    27 jun

--- distance.py
+++ distance.py
@@ -6,6 +6,6 @@ sds = geodesic((4.667982,-74.103693),(5.465616,-74.653929))
 sds = str(sds)
 sds = sds.split(' ')
 distancia = float(sds[0])
-dis = distancia * 1.60934
+di = distancia * 1.60934
 
-print(dis)
+print(di)
